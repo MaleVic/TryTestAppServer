d5de1e61c0d5e6ce065442d435ed3ae383f577e3
Viktor Maleev <gamesmannna@mail.ru>
2018-06-18 01:01:18 +0300

    Реализована авторизация на сервер

---
+++ app/src/main/java/com/example/gamesmannna/trytestappserver/Common/Common.java
@@ -0,0 +1,11 @@
+package com.example.gamesmannna.trytestappserver.Common;
+
+import com.example.gamesmannna.trytestappserver.Model.User;
+
+/**
+ * Created by Gamesmannna on 17.06.2018.
+ */
+
+public class Common {
+    public static User currentUser;
+}
--- app/src/main/java/com/example/gamesmannna/trytestappserver/MainActivity.java
+++ app/src/main/java/com/example/gamesmannna/trytestappserver/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.gamesmannna.trytestappserver;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button SignInButton;
+    TextView txtSlogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignInButton = (Button) findViewById(R.id.SignInButton);
+        txtSlogan = (TextView) findViewById(R.id.txtSlogan);
+
+
+        SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
     }
 }
